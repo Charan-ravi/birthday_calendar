c53b178660463d486e6231bc9f622bb4ee57c085
Charan-ravi <charan2ravi@gmail.com>
2022-03-30 14:10:05 +0530

    saving...

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const data = [
+  { id: 1, name: "ben" },
+  { id: 2, name: "parker" },
+  { id: 3, name: "harry" },
+  { id: 4, name: "may" },
+  { id: 5, name: "osborn" },
+  { id: 6, name: "gwen" },
+];
